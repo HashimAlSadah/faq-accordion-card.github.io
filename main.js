d868c9b1243ca278617fe6da8ffea969b57c334c
HashimAlSada <HashimAsada2894@gmail.com>
2022-05-23 22:13:53 +0300

    added transition to the text appearence

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const card = document.querySelector(".main");
 const answers = document.querySelectorAll(".question p")
 
-//---Make the answers disappear--------- 
+// ---Make the answers disappear--------- 
 answers.forEach(element => {
-    element.style.display = "none";
+    element.style.height = "0px";
+    element.style.opacity = "0";
 })
 
 //-----clicking the question events--------------
@@ -11,14 +12,16 @@ card.addEventListener("click", (event)=>{
     if(event.target.classList.contains("q-statment")){
         let textAnswer = event.target.parentNode.children[1];
         let arrow = event.target.children[1];
-        
-        if (textAnswer.style.display == "none"){
-            textAnswer.style.display = "block";
+    
+        if (textAnswer.style.height == "0px"){
+            textAnswer.style.height = "100%"
+            textAnswer.style.opacity = "1"
             event.target.classList.add("clicked");
             arrow.style.transform = "rotate(180deg)";
         }
         else{
-            textAnswer.style.display = "none";
+            textAnswer.style.height = "0px";
+            textAnswer.style.opacity = "0"
             event.target.classList.remove("clicked");
             arrow.style.transform = "rotate(0deg)";
         }
